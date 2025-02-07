38be63898f30cb3a19e85cf0177d6ae5107270a5
aleixo-dev <nicolasaleixo2020@gmail.com>
2025-02-07 16:45:33 -0300

    build: add ktor client dependencies

--- minecraft/plugins/mine-relay/build.gradle.kts
+++ minecraft/plugins/mine-relay/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("java-library")
     alias(libs.plugins.jetbrains.kotlin.jvm)
     id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.1.0"
 }
 
 group = "com.nicolas"
@@ -26,6 +27,11 @@ repositories {
 dependencies {
     compileOnly(libs.paper.api.v1214r01snapshot)
     implementation(libs.kotlin.stdlib.jdk8)
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.cio)
+    implementation(libs.ktor.serialization.kotlinx.json)
+    implementation(libs.ktor.client.content.negotiation)
+    implementation(libs.kotlinx.serialization.json)
 }
 
 val targetJavaVersion = 21
@@ -35,4 +41,19 @@ kotlin {
 
 java {
     toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+}
+
+tasks.build {
+    dependsOn("shadowJar")
+}
+
+tasks.withType<Jar> {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+}
+
+tasks.shadowJar {
+    archiveBaseName = "MineRelay"
+    archiveVersion = ""
+    archiveClassifier = ""
+    destinationDirectory.set(file("C:/Users/nicol/OneDrive/Área de Trabalho/ARQUIVOS/MINECRAFR_1_21_4_SERVER/plugins"))
 }
